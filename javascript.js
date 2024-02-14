4754c43fb4cd4ca125938b23b5d66c80b6ee6a9f
AhmedWaleedKalifa <ahmed2210waleed@gmail.com>
2024-02-14 18:26:24 +0200

    Feat:make user can make an operation with the result with the second number

--- javascript.js
+++ javascript.js
@@ -10,7 +10,7 @@ function multiply(num1,num2){
 function divide(num1,num2){
     return (num1/num2);
 }
-function operation(num1,operation,num2){
+function operation(num1="",operation,num2){
 let sum;
 switch(operation){
     case "+":
@@ -23,17 +23,14 @@ switch(operation){
         sum=multiply(num1,num2);
         break;
     case "/":
-        if(num2==0){
-            return "Error";
-        }
         sum=divide(num1,num2);
         break;
 }
 result=sum;
 return sum;
 }
-let numbers=[0,0];
-let result=0;
+let numbers=["",""];
+let result;
 let counter=0;
 let op="";
 const display=document.querySelector("#display");
@@ -42,32 +39,59 @@ const operations=document.querySelectorAll(".operation");
 const equal=document.querySelector(".equals");
 buttons.forEach((button)=>{
     button.addEventListener("click",(element)=>{
-        numbers[counter]=display.textContent=element.target.getAttribute("id");
-        counter++;
+        const lastElement=display.textContent.at(-1);
+        if(lastElement=="+"|lastElement=="-"|lastElement=="/"|lastElement=="*"){
+            display.textContent="";
+            counter++;
+        }else if(display.textContent.at(0)=="="){
+            display.textContent="";
+        }
+        display.textContent=display.textContent+element.target.getAttribute("id");
+        numbers[counter]+=element.target.getAttribute("id");
     })
 })
 operations.forEach((o)=>{
     o.addEventListener("click",(element)=>{
         op=display.textContent=element.target.getAttribute("id");
-    })
+        //counter++;
+    });
 })
 equal.addEventListener("click",(element)=>{
-    if(op=="/"){
-        display.textContent="= "+(operation(numbers[0],op,numbers[1])).toFixed(3);
+    
+    if(numbers[0]!=""){
+        result=(operation(numbers[0],op,numbers[1]));
+        if(op=="/"){
+            if(numbers[1]==0){
+                display.textContent="Error";
+            }
+            display.textContent="= "+result.toFixed(3);
+        }else{
+            display.textContent="= "+result;
+        }
     }else{
-        display.textContent="= "+(operation(numbers[0],op,numbers[1]));
+        console.log(result);
+        display.textContent="= "+(operation(result,op,numbers[1]));
     }
     counter=0;
+    numbers[0]="";
+    numbers[1]="";
 
 });
 const ans=document.querySelector(".ans");
-ans.addEventListener("click",()=>{
-    display.textContent=result;
-    numbers[counter]=result;
-    counter++;
-})
+// ans.addEventListener("click",()=>{
+//     display.textContent=result;
+//     numbers[counter]=result;
+//     counter++;
+// })
 const clear=document.querySelector(".clear");
 clear.addEventListener("click",()=>{
     display.textContent="";
     counter=0;
 })
+/*
+
+ if(element.target.getAttribute("class")=="num"){
+            display.textContent=display.textContent+element.target.getAttribute("id");
+            numbers[counter]+=numbers[counter];
+        }
+*/
